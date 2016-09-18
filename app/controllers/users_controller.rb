a03cfe520b1fa18dd0e55288426e30022d7caf8a
岩井　智夢 <tomo42695@gmail.com>
2016-09-18 15:54:00 +0900

    edit_users実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def show
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.id == current_user.id
+      user.update(user_params)
+    end
+  end
+
+  def user_params
+    params.permit(:email, :password, :username, :member, :profile, :work, :image)
+  end
 end
