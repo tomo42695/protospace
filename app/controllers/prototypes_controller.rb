64fc413442b6fdc92906014ee680b2eea3fbd93f
岩井　智夢 <tomo42695@gmail.com>
2016-09-24 20:14:39 +0900

    prototype_create

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,17 @@
 class PrototypesController < ApplicationController
   def index
   end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    Prototype.create(prototype_params)
+    redirect_to action: :index
+  end
+
+  def prototype_params
+    params.permit(:title, :text, :catchcopy, :concept)
+  end
 end
