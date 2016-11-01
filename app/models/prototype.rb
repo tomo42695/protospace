fad8cde5c0c5ccd93869bf07013196bb2ead1102
tomo42695 <tomo42695@gmail.com>
2016-11-01 19:48:24 +0900

    dependent編集

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,12 +1,12 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :prototype_images
-  has_many :likes
+  has_many :likes, dependent: :destroy
   accepts_nested_attributes_for :prototype_images, allow_destroy: true, reject_if: proc { |attributes| attributes['content'].blank? }
   validates :title, :catchcopy, :concept, :prototype_images, presence: true
   paginates_per 8
 
-  def like_user(user_id)
-    likes.find_by(user_id: user_id)
+  def user_liked?(user)
+    likes.find_by(user_id: user)
   end
 end
