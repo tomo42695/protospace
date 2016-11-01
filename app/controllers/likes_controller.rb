e957cd3efe5d423560fd71fdf5b6b56ba18dc4d3
tomo42695 <tomo42695@gmail.com>
2016-11-01 19:16:29 +0900

    delete_files_and_create_set_variables

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,16 @@
 class LikesController < ApplicationController
   def create
     @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
-    @likes = Like.where(prototype_id: params[:prototype_id])
-    @prototype = Prototype.find(params[:prototype_id])
+    set_variables
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
     like.destroy
+    set_variables
+  end
+
+  def set_variables
     @likes = Like.where(prototype_id: params[:prototype_id])
     @prototype = Prototype.find(params[:prototype_id])
   end
