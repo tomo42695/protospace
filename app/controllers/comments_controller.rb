1ec46c38b2da20cf493174b492cebf09b859de7f
岩井　智夢 <tomo42695@gmail.com>
2016-11-07 19:17:59 +0900

    done editing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,20 @@
 class CommentsController < ApplicationController
+  before_action :set_prototype
   def create
-    @prototype = Prototype.find(params[:prototype_id])
     Comment.create(comment_params)
   end
 
   def destroy
     comment = Comment.find(params[:id])
     comment.destroy
-    @prototype = Prototype.find(params[:prototype_id])
   end
 
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: @prototype.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
   end
 end
